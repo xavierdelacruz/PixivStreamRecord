1cfea25483668e1aab9b22b2d1a8aa049250b039
xavierdelacruz <xdelacruz89@gmail.com>
2020-12-31 04:05:12 -0800

    added more changes to suppor tprivate streams

--- PixivStreamRecord/AnlyStreamUrl.py
+++ PixivStreamRecord/AnlyStreamUrl.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import json
 import sys,os,signal
 import time
+import subprocess
 
 def getStreamUrl(id):
     http = urllib3.PoolManager()
@@ -11,8 +12,8 @@ def getStreamUrl(id):
     # user2 sakusyo-art
     # user3 katze_horenso
     url = 'https://sketch.pixiv.net/@%s/lives/' %id
-    r = http.request('GET', url)
-    soup = BeautifulSoup(r.data.decode('utf-8'), "lxml")
+    r = http.request('GET', url)    
+    soup = BeautifulSoup(r.data.decode('utf-8'), "lxml") 
     x = soup.find(id='state').string
     x = x[x.find('=') + 1:]
     x = x.split('\n')[0].rstrip(';').replace('undefined', 'null')
@@ -41,47 +42,65 @@ def getHighResUrl(id):
         return False,False
 
 if __name__ == "__main__":
-    userid = sys.argv[1]
-    sleep_seconds = int(sys.argv[2])
-    dateTime = time.strftime('%y%m%d%H%M',time.localtime(time.time()))
+    userid = sys.argv[1]  
+    sessionid = sys.argv[2]
+    devicetoken = sys.argv[3]
+    sleep_seconds = int(sys.argv[4])
     highResUrl, live_id = getHighResUrl(userid)
 
     filenum = 0
+    useStreamlink = False
     while(1):
+        dateTime = time.strftime('%y%m%d%H%M',time.localtime(time.time()))
         highResUrl = None
         live_id = None
         while not live_id:
             highResUrl, live_id = getHighResUrl(userid)
             if live_id:
                 break
-            print(f'Live has not started, sleeping for {sleep_seconds} seconds')
+            else:
+                bad = 'No playable'
+                cmd = f'streamlink --pixiv-sessionid {sessionid} --pixiv-devicetoken {devicetoken} https://sketch.pixiv.net/@{userid}/lives --stream-url'
+                out = os.popen(cmd).read()
+                if  bad not in out:
+                    useStreamlink = True
+                    break              
+            print(f'Live stream has not started, sleeping for {sleep_seconds} seconds')
             time.sleep(sleep_seconds)
+        # Start process here
         filenum += 1
-        print('URL = %s , live_id = %s'%(highResUrl,live_id))
-        fileName = 'PixivStream-%s-%s-%s-%s' %(userid,dateTime,live_id,filenum)
-        print('logFileName = %s.log , streamFileName = %s.mkv'%(fileName,fileName))
-        processInfo = os.popen('ps -ef |grep %s |grep -v grep'%live_id).readlines()
-        processNum = len(processInfo)
-        print('processNum = %s'%processNum)
-        if processNum==0:
-            target_dir = 'output/'
-            if not os.path.exists('output/'):
-                os.mkdir(target_dir)
-            os.system(f'nohup /usr/bin/ffmpeg -i {highResUrl} -c copy {target_dir}{fileName}.mkv >{target_dir}{fileName}.log 2>&1 &')
-            print('Stream start Recording')
-            while(1):
-                time.sleep(60)
-                http = urllib3.PoolManager()
-                urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-                resCode = http.request('GET', highResUrl)
-                if resCode.status == 200:
-                    print('resCode = %s , Stream is still alive from %s'%(resCode.status,userid))
-                    continue
-                else:
-                    processPids = os.popen("ps -ef |grep %s |grep -v grep|awk '{print $2}'" %live_id).readlines()
-                    for pid in processPids:
-                        print('pid = %s , Stream record is dead' %pid)
-                        os.kill(int(pid),signal.SIGKILL)
-                    break
+        if useStreamlink:
+            print('Using Streamlink library to record stream now')
+            fileName = 'PixivStream-Streamlink-%s-%s-%s'%(userid,dateTime,filenum)
+            cmd = f'streamlink --pixiv-sessionid {sessionid} --pixiv-devicetoken {devicetoken} https://sketch.pixiv.net/@{userid}/lives best --output output/{fileName}.mkv'
+            bad = 'No playable'
+            os.system(cmd)
         else:
-            print('Stream is Recording')
+            print('URL = %s , live_id = %s'%(highResUrl,live_id))
+            fileName = 'PixivStream-%s-%s-%s-%s' %(userid,dateTime,live_id,filenum)
+            print('logFileName = %s.log , streamFileName = %s.mkv'%(fileName,fileName))
+            processInfo = os.popen('ps -ef |grep %s |grep -v grep'%live_id).readlines()
+            processNum = len(processInfo)
+            print('processNum = %s'%processNum)
+            if processNum==0:
+                target_dir = 'output/'
+                if not os.path.exists('output/'):
+                    os.mkdir(target_dir)
+                os.system(f'nohup /usr/bin/ffmpeg -i {highResUrl} -c copy {target_dir}{fileName}.mkv >{target_dir}{fileName}.log 2>&1 &')
+                print('Stream start Recording')
+                while(1):
+                    time.sleep(60)
+                    http = urllib3.PoolManager()
+                    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+                    resCode = http.request('GET', highResUrl)
+                    if resCode.status == 200:
+                        print('resCode = %s , Stream is still alive from %s'%(resCode.status,userid))
+                        continue
+                    else:
+                        processPids = os.popen("ps -ef |grep %s |grep -v grep|awk '{print $2}'" %live_id).readlines()
+                        for pid in processPids:
+                            print('pid = %s , Stream record is dead' %pid)
+                            os.kill(int(pid),signal.SIGKILL)
+                        break
+            else:
+                print('Stream is Recording')
--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Requires Python3.8
 Setup
 ```
 
-# Windows-Users
+# Windows-Users wanting to use Linux
 ```
 $ wsl --list                               # List all installed builds in Windows
 $ Choose a Distro on the Microsft Store
@@ -16,18 +16,26 @@ $ Open whichever distro you installed (i.e. type Ubuntu if you installed it)
 $ Terminal will open, and it will prompt you for a password and user (root user priv)
 $ update and upgrade your current Distro
 
-# First time only
+# Virtual Env Setup (OPTIONAL)
 ```
 $ python3 -m venv venv                     # Do not use with the Windows Linux Subsystem
 $ source venv/bin/activate                 # MacOS, Linux (Do not use the Windows Linux Subsystem)
 $ venv\Scripts\activate.bat                # on Windows
+
+# Installing dependencies
+```
 $ pip3 install -r requirements.txt         # Install dependencies
 
-# Any other time
+# Virtual Env Usage (OPTIONAL)
 ```
-$ source venv/bin/activate                 # MacOS, Linux (Do not use the Windows subsystem)
-$ venv\Scripts\activate.bat                # on Windows
-$ python3 AnlyStreamUrl.py <id> <poll_time> # to run
+$ source venv/bin/activate                  # MacOS, Linux (Do not use the Windows subsystem)
+$ venv\Scripts\activate.bat                 # on Windows
+
+# Running the program with PowerShell, Bash, Mac Terminals, or Linux Terminals
+```
+$ python3 AnlyStreamUrl.py <id> <session_id> <device_token> <poll_time> # to run
 
 The `<id>` is part of the pixiv URL, e.g.: `https://sketch.pixiv.net/@<id>/lives/`
+The `<session_id>` is the value specified in PHPSESSID of your pixiv cookies
+The `<device_token>` is the value specified in device_token of your pixiv cookies
 The `<poll_time>` is the time in seconds to poll if the stream is live
